8197fccc5751304beb1220c6424a95f8c691aa16
brandon.liu.72@gmail.com <brandon.liu.72@gmail.com>
2019-06-02 23:14:07 -0700

    updated

--- todolist/app.js
+++ todolist/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 
 // New Code
 var monk = require('monk');
--- todolist/routes/index.js
+++ todolist/routes/index.js
@@ -10,13 +10,48 @@ router.get('/', function (req, res, next) {
 router.get('/todolist', function (req, res) {
     var db = req.db;
     var collection = db.get('todocollection');
+
     collection.find({}, {}, function (e, docs) {
         res.render('todolist', {
             "todolist": docs
         });
-        console.log(collection)
     });
 });
 
+router.post('/todolist', function (req, res) {
+    var db = req.db;
+    var collection = db.get('todocollection');
+
+    var todo = req.body.todo;
+    var time = req.body.time;
+
+    collection.insert({
+        "todo": todo,
+        "time": time
+    }, function (err, docs) {
+        if (err) {
+            res.send('Error sending data to db');
+        } else {
+            res.redirect('/todolist');
+        }
+    });
+});
+
+router.get('/todolist/:id', function (req, res) {
+    var db = req.db;
+    var collection = db.get('todocollection');
+
+    var todoToDelete = req.params.id;
+
+    collection.remove({
+        "_id": todoToDelete
+    }, function (err, docs) {
+        if (err) {
+            res.send('Error deleting data in db');
+        } else {
+            res.redirect('/todolist');
+        }
+    });
+});
 
 module.exports = router;
